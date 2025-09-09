44fd4d4304f5c8ae1f5e696663f1cc91471bc103
Guzman Edilberto <guzmanedi5825@gmail.com>
2025-09-09 07:05:41 -0300

    agregacion del index.css de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import Inicio from './pages/inicio.jsx'
 import Contacto from './pages/contacto.jsx'
 import Hospitales from './pages/hospitales.jsx'
+import './index.css'
+
 
 function App() {
   return (
     <div>
+
+      <div> 
+        <h1 className="text-3xl font-bold underline text-center p-4 bg-blue-200 text-blue-900">
+          Directorio de Hospitales del Gran Buenos Aires</h1>
+      </div>
+
       <Routes>
         <Route path='/' element={<Inicio />} />
         <Route path='/contacto' element={<Contacto />} />
