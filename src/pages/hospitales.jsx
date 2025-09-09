bf17f40de2d89de853cf2bcb4ccff844f41fa4bd
Guzman Edilberto <guzmanedi5825@gmail.com>
2025-09-09 06:40:38 -0300

    creacion de las paginas

---
+++ src/pages/hospitales.jsx
@@ -0,0 +1,9 @@
+function Hospitales() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Hospitales en el Gran Buenos Aires</h1>
+      <p className="text-gray-700">Aquí puedes encontrar información sobre los hospitales en el Gran Buenos Aires.</p>
+    </div>
+  )
+}
+export default Hospitales
---
+++ src/pages/inicio.jsx
@@ -0,0 +1,10 @@
+function Inicio() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Bienvenido a la página de Inicio</h1>
+      <p className="text-gray-700">Esta es la página principal de nuestro sitio web.</p>
+    </div>
+  )
+}
+
+export default Inicio
