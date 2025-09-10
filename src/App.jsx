5b8bd4574aa72fb0bfca0e7b38c7efa7d6e013e7
Guzman Edilberto <guzmanedi5825@gmail.com>
2025-09-10 07:22:18 -0300

    creacion base de la pagina de formularios de establecimientos

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contacto from './pages/contacto.jsx'
 import Hospitales from './pages/hospitales.jsx'
 import HospitalDetalle from "./pages/hospitalDetalle.jsx"
 import './index.css'
-
+import FormularioEstablecimiento from "./pages/formularioEstablecimiento.jsx"
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         <Route path='/' element={<Inicio />} />
         <Route path='/contacto' element={<Contacto />} />
         <Route path='/hospitales' element={<Hospitales />} />
+        <Route path='/formularioEstablecimiento' element={<FormularioEstablecimiento/>} />
         <Route path="/hospitales/:nombre" element={<HospitalDetalle />} />
       </Routes>
     </div>
