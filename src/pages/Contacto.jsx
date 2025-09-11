4cd4e2a5fed85855bb964b6b431f3e41a0129cca
Guzman Edilberto <guzmanedi5825@gmail.com>
2025-09-10 22:32:48 -0300

    reingreso de los mismo archivos pero con nombre Pascal

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,13 @@
+function Contacto() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Página de Contacto</h1>
+      <p className="text-gray-700">Puedes contactarnos a través del siguiente formulario.</p>
+
+      
+
+
+    </div>
+  )
+}
+export default Contacto
---
+++ src/pages/FormularioEstablecimiento.jsx
@@ -0,0 +1,9 @@
+function FormularioEstablecimiento() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Página de establecimiento</h1>
+      <p className="text-gray-700"> esta pagina se agregara una especie de formulario para ingresar nuevos centro medicos en la base de datos.</p>
+    </div>
+  )
+}
+export default FormularioEstablecimiento
