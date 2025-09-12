16074b2e50f6269a3bc8c21ba5c48a1d960f027d
Guzman Edilberto <guzmanedi5825@gmail.com>
2025-09-12 06:54:38 -0300

    cambios en el formato de enlace

--- src/componentes/HospitalRow.jsx
+++ src/componentes/HospitalRow.jsx
@@ -32,7 +32,12 @@ export default function HospitalRow({ nombre, ubicacion, especialidades }) {
               
               }
             </ul>
-            <Link to={`/hospitales/${nombre}`}>enlace</Link>
+            <Link
+              to={`/hospitales/${nombre}`}
+              className="inline-block mt-3 px-4 py-2 bg-gray-600 text-white rounded hover:bg-green-700 transition"
+            >
+              Ver detalles
+            </Link>
           </td>
         </tr>
       )}
